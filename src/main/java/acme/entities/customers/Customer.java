b5ae90df27b2b785e7f17622d392482b5edcc121
Javclamar <javclamar@alum.us.es>
2025-03-02 16:51:43 +0100

    Fix: Añade la anotación Automapped a Customer
    Añade la anotación Automapped a los atributos necesarios de la clase Customer

--- src/main/java/acme/entities/customers/Customer.java
+++ src/main/java/acme/entities/customers/Customer.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
@@ -26,22 +27,27 @@ public class Customer extends AbstractEntity {
 	private String				identifier;
 
 	@Mandatory
+	@Automapped
 	@ValidString(pattern = "^\\+?\\d{6,15}$")
 	private String				phone;
 
 	@Mandatory
+	@Automapped
 	@ValidString(min = 0, max = 255)
 	private String				physicalAddress;
 
 	@Mandatory
+	@Automapped
 	@ValidString(min = 0, max = 50)
 	private String				city;
 
 	@Mandatory
+	@Automapped
 	@ValidString(min = 0, max = 50)
 	private String				country;
 
 	@Optional
+	@Automapped
 	@ValidNumber(min = 0, max = 500000)
 	private Integer				points;
 
