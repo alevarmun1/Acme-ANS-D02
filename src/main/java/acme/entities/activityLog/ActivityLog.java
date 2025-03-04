d426dfad8a7c4443ece44d0cd8d82fcbd970ce7a
AngSanRui <angsanrui2@alum.us.es>
2025-03-04 22:56:50 +0100

    S3M5 Activity log hecho

---
+++ src/main/java/acme/entities/activityLog/ActivityLog.java
@@ -0,0 +1,45 @@
+
+package acme.entities.activityLog;
+
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+
+public class ActivityLog extends AbstractEntity {
+
+	// Serialisation identifier
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				typeOfIncident;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Integer				severityLevel;
+
+	// Derived attributes
+
+	// Relationships
+}
