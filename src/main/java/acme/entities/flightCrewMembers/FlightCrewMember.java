1d89372056381cc9d049d870227a1741f5058cf6
AngSanRui <angsanrui2@alum.us.es>
2025-03-04 12:53:44 +0100

    minifix flight crew members

--- src/main/java/acme/entities/flightCrewMembers/FlightCrewMember.java
+++ src/main/java/acme/entities/flightCrewMembers/FlightCrewMember.java
@@ -1,18 +1,13 @@
 
 package acme.entities.flightCrewMembers;
 
-import java.util.Date;
-
 import javax.persistence.Column;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
-import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
@@ -58,31 +53,6 @@ public class FlightCrewMember extends AbstractEntity {
 	@ValidNumber(min = 0)
 	@Automapped
 	private Integer				yearsOfExperience;
-
-	@Mandatory
-	@ValidMoment(past = true)
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date				moment;
-
-	@Mandatory
-	@ValidString(max = 50)
-	@Automapped
-	private String				subject;
-
-	@Mandatory
-	@ValidString(max = 255)
-	@Automapped
-	private String				text;
-
-	@Optional
-	@ValidNumber(max = 10, min = 0)
-	@Automapped
-	private Double				score;
-
-	@Optional
-	//@Valid
-	@Automapped
-	private Boolean				recommended;
 
 	// Derived attributes
 
