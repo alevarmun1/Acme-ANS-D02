94f3bf65fb8c0f750b73b35169b0658b776ef8b2
AngSanRui <angsanrui2@alum.us.es>
2025-03-04 12:52:12 +0100

    S3M3 flight crew members hecho

--- src/main/java/acme/entities/flightCrewMembers/FlightCrewMember.java
+++ src/main/java/acme/entities/flightCrewMembers/FlightCrewMember.java
@@ -1,29 +1,63 @@
 
-package acme.entities.review;
+package acme.entities.flightCrewMembers;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airlines.Airline;
 
-public class Review extends AbstractEntity {
+public class FlightCrewMember extends AbstractEntity {
 
 	// Serialisation identifier
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes
 	@Mandatory
-	@ValidString(max = 50)
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
 	@Automapped
-	private String				name;
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				languageSkills;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private AvailabilityStatus	availabilityStatus;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Airline				airline;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Airline				salary;
+
+	@Optional
+	@ValidNumber(min = 0)
+	@Automapped
+	private Integer				yearsOfExperience;
 
 	@Mandatory
 	@ValidMoment(past = true)
