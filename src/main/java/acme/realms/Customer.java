337d92dc42551d23b84a9dbdb6313cf78bde86f4
Javclamar <javclamar@alum.us.es>
2025-03-05 23:56:05 +0100

    Pasa Customer a ser un realm
    Pasa la clase Customer al paquete realms y ahora extiende de AbstractRole

--- src/main/java/acme/realms/Customer.java
+++ src/main/java/acme/realms/Customer.java
@@ -1,10 +1,10 @@
 
-package acme.entities.customers;
+package acme.realms;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -16,34 +16,34 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Customer extends AbstractEntity {
+public class Customer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes
 	@Mandatory
 	@Column(unique = true)
-	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidString(min = 1, max = 9, pattern = "^[A-Z]{2,3}\\d{6}$")
 	private String				identifier;
 
 	@Mandatory
 	@Automapped
-	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@ValidString(min = 6, max = 15, pattern = "^\\+?\\d{6,15}$")
 	private String				phone;
 
 	@Mandatory
 	@Automapped
-	@ValidString(min = 0, max = 255)
+	@ValidString(min = 1, max = 255)
 	private String				physicalAddress;
 
 	@Mandatory
 	@Automapped
-	@ValidString(min = 0, max = 50)
+	@ValidString(min = 1, max = 50)
 	private String				city;
 
 	@Mandatory
 	@Automapped
-	@ValidString(min = 0, max = 50)
+	@ValidString(min = 1, max = 50)
 	private String				country;
 
 	@Optional
