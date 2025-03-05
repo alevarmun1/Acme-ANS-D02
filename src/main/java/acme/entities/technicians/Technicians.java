5090e9323c517ca7ce4381b6da379729d69f09e6
Rubén López Expósito <rublopexp@alum.us.es>
2025-03-05 10:47:16 +0100

    S5M3-Technicians

---
+++ src/main/java/acme/entities/technicians/Technicians.java
@@ -0,0 +1,58 @@
+
+package acme.entities.technicians;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Technicians extends AbstractRole {
+
+	// Serialisation identifier
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				licenseNumber;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				specialisation;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				annualHealthTestPassed;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private Integer				experienceYears;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				certifications;
+
+}
