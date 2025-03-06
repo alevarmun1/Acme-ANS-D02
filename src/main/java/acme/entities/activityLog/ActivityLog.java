ba3f9f8f528553a188f6f99c031ef75bc6934282
AngSanRui <angsanrui2@alum.us.es>
2025-03-06 13:31:21 +0100

    cambios en csv y en clases de student 3 y review, principalmente relaciones añadidas

--- src/main/java/acme/entities/activityLog/ActivityLog.java
+++ src/main/java/acme/entities/activityLog/ActivityLog.java
@@ -4,8 +4,10 @@ package acme.entities.activityLog;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -13,6 +15,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.flightAssignment.FlightAssignment;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,8 +28,9 @@ public class ActivityLog extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes
+
 	@Mandatory
-	@ValidMoment(past = true)
+	@ValidMoment(past = true)			//validar que el momento sea después de la finalización del leg
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				registrationMoment;
 
@@ -48,4 +52,9 @@ public class ActivityLog extends AbstractEntity {
 	// Derived attributes
 
 	// Relationships
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private FlightAssignment	assignment;
 }
